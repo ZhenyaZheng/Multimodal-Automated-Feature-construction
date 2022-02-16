d2d84f77ca6ce27f772ea0a0d390a271ca70217a
zzy728772024 <728772024@qq.com>
2022-02-16 21:07:45 +0800

    Dataset

--- Dataset.py
+++ Dataset.py
@@ -1,6 +1,9 @@
 
 class Dataset:
     def __init__(self,data_path = None):
+        '''
+        :param data_path:数据集路径，是一个字典:{'image_path':"",'text_path':"",'tabular_path':""}
+        '''
         self.data_path = data_path
         self.data_tabular = self.read_tabular()
         self.data_text = self.read_text()
@@ -9,10 +12,25 @@ class Dataset:
 
 
     def read_text(self):
+        '''
+
+        :return:Dask.Dataframe
+        '''
         pass
 
     def read_image(self):
+        '''
+
+        :return:cv2
+        '''
         pass
 
-    def read_form(self):
+    def read_tabular(self):
+        '''
+
+        :return:Dask.Dataframe
+        '''
+        pass
+
+    def __copy__(self):
         pass
