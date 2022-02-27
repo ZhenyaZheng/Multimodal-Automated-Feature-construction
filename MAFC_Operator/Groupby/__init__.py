b6773d33aeb1ad1ba0eb7aea608fb6639a3dc5cd
zzy728772024 <728772024@qq.com>
2022-02-27 10:51:53 +0800

    Operators

---
+++ MAFC_Operator/Groupby/__init__.py
@@ -0,0 +1,6 @@
+from MAFC_Operator.Groupby.groupby import *
+from MAFC_Operator.Groupby.GroupMax import *
+from MAFC_Operator.Groupby.GroupMin import *
+from MAFC_Operator.Groupby.GroupAvg import *
+from MAFC_Operator.Groupby.GroupCount import *
+from MAFC_Operator.Groupby.GroupStd import *
