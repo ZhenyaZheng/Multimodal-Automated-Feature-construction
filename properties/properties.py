7ef89b6b531278b35c05b436ba7f5bdbd2d9d36d
zzy728772024 <728772024@qq.com>
2022-03-13 16:48:31 +0800

    properties

--- properties/properties.py
+++ properties/properties.py
@@ -1,9 +1,11 @@
 
-class propreties:
+class properties:
     def __init__(self):
         self.unaryoperator = ['DayofWeek','Discretizer','HourofDay','IsWeekend','StdOperator']
         self.otheroperator = ['AddOperator','SubOperator','MultiOperator','DivisOperator','GroupMax','GroupMean','GroupMin','GroupStd','GroupCount']
         self.DiscretizerBinsNumber = 10 #将实数离散化后的类数
         self.maxcombination = 2
+        self.classifier='RandomForest'
+        self.DatasetInstancesFilesLocation = "/"
 
-theproperty = propreties()
+theproperty = properties()
