0241658eb8cf21fe26978b5ebe33dbd27bb104d4
zzy728772024 <728772024@qq.com>
2022-03-24 23:05:53 +0800

    properties

--- properties/properties.py
+++ properties/properties.py
@@ -5,7 +5,15 @@ class properties:
         self.otheroperator = ['AddOperator','SubOperator','MultiOperator','DivisOperator','GroupMax','GroupMean','GroupMin','GroupStd','GroupCount']
         self.DiscretizerBinsNumber = 10 #将实数离散化后的类数
         self.maxcombination = 2
-        self.classifier='RandomForest'
-        self.DatasetInstancesFilesLocation = "/"
-
+        self.classifier = 'RandomForest'
+        self.datasetlocation = "data/datasets/"
+        self.filter = "MLFEvaluation"
+        self.wrapper = "AucWrapperEvaluation"
+        self.thread = 1
+        self.fsocre = 0.001
+        self.maxevaluationattsperiter = 15000
+        self.backmodelpath = "data/model/"
+        self.datasetname = "dataset"
+        self.classifiersforMLAttributes = ["RandomForest"]
+        self.targetindex = -1
 theproperty = properties()
